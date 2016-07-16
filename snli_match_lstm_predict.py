e4fb6090c44518ffc0b6022bd895d52a2408de7d
junfenglx <junfenglx@gmail.com>
2016-07-16 17:08:47 +0800

    add a predict program.

---
+++ snli_match_lstm_predict.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+
+# Created by junfeng on 7/16/16.
+
+# logging config
+
+import logging
+import pickle
+import sys
+import time
+
+import numpy
+import numpy as np
+import pandas as pd
+
+import theano
+import theano.tensor as T
+import lasagne
+from nltk import word_tokenize
+
+from custom_layers import CustomEmbedding, MatchLSTM, FakeFeatureDot2Layer
+
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p',
+                    level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+class MatchLSTMModel(object):
+    LABEL2NAME = {0: 'neutral',
+                  1: 'contradiction',
+                  2: 'entailment'}
+
+    def __init__(self,
+                 dict_file='./snli/dictionary.pkl',
+                 model_file='./snli/mlstm_model.npz',
+                 unchanged_W_file='./snli/unchanged_W.pkl',
+                 oov_in_train_file='./snli/oov_in_train_W.pkl',
+                 k=300, p=0.3):
+        self.dict_file = dict_file
+        self.model_file = model_file
+        self.unchanged_W_file = unchanged_W_file
+        self.oov_in_train_file = oov_in_train_file
+        self.k = k
+        self.p = p
+
+        self.dictionary = None
+        self.predict_fn = None
+
+        self.load_data()
+        self.build_model()
+
+    def load_data(self):
+        with open(self.dict_file, 'rb') as f:
+            self.dictionary = pickle.load(f)
+
+    def word2id(self, sentence):
+        sent = []
+        words = word_tokenize(sentence)
+
+        for w in words:
+            if w in self.dictionary:
+                sent.append(self.dictionary[w])
+            else:
+                print('Warning: {} not in dictionary'.format(w))
+        return sent
+
+    def prepare(self, samples):
+        seqs_premise = []
+        seqs_hypothesis = []
+        for i, (p, h) in enumerate(samples, start=1):
+            p_ids = self.word2id(p)
+            h_ids = self.word2id(h)
+            if not (p_ids and h_ids):
+                print('sample {} has empty sentence, skiped'.format(i))
+                continue
+            seqs_premise.append(p_ids)
+            seqs_hypothesis.append(h_ids)
+        seqs_p = seqs_premise
+        seqs_h = seqs_hypothesis
+
+        lengths_p = [len(s) for s in seqs_p]
+        lengths_h = [len(s) for s in seqs_h]
+
+        n_samples = len(seqs_p)
+        maxlen_p = numpy.max(lengths_p) + 1
+        maxlen_h = numpy.max(lengths_h) + 1
+
+        premise = numpy.zeros((n_samples, maxlen_p), dtype='int32')
+        hypothesis = numpy.zeros((n_samples, maxlen_h), dtype='int32')
+        premise_masks = numpy.zeros((n_samples, maxlen_p), dtype='int32')
+        hypothesis_masks = numpy.zeros((n_samples, maxlen_h), dtype='int32')
+        for idx, [s_t, s_h] in enumerate(zip(seqs_p, seqs_h)):
+            assert lengths_h[idx] == len(s_h)
+            premise[idx, :lengths_p[idx]] = s_t
+            premise_masks[idx, :lengths_p[idx]] = 1
+            hypothesis[idx, :lengths_h[idx]] = s_h
+            hypothesis_masks[idx, :lengths_h[idx]] = 1
+
+        return (premise, premise_masks,
+                hypothesis, hypothesis_masks)
+
+    def build_model(self):
+        premise_max = 82 + 1
+        hypothesis_max = 62 + 1
+
+        print("Building network ...")
+        premise_var = T.imatrix('premise_var')
+        premise_mask = T.imatrix('premise_mask')
+        hypo_var = T.imatrix('hypo_var')
+        hypo_mask = T.imatrix('hypo_mask')
+        unchanged_W = pickle.load(open(self.unchanged_W_file, 'rb'))
+        unchanged_W = unchanged_W.astype('float32')
+        unchanged_W_shape = unchanged_W.shape
+        oov_in_train_W = pickle.load(open(self.oov_in_train_file, 'rb'))
+        oov_in_train_W = oov_in_train_W.astype('float32')
+        oov_in_train_W_shape = oov_in_train_W.shape
+        print('unchanged_W.shape: {0}'.format(unchanged_W_shape))
+        print('oov_in_train_W.shape: {0}'.format(oov_in_train_W_shape))
+
+        l_premise = lasagne.layers.InputLayer(shape=(None, premise_max), input_var=premise_var)
+        l_premise_mask = lasagne.layers.InputLayer(shape=(None, premise_max), input_var=premise_mask)
+        l_hypo = lasagne.layers.InputLayer(shape=(None, hypothesis_max), input_var=hypo_var)
+        l_hypo_mask = lasagne.layers.InputLayer(shape=(None, hypothesis_max), input_var=hypo_mask)
+
+        premise_embedding = CustomEmbedding(l_premise, unchanged_W, unchanged_W_shape,
+                                            oov_in_train_W, oov_in_train_W_shape,
+                                            p=self.p)
+        # weights shared with premise_embedding
+        hypo_embedding = CustomEmbedding(l_hypo, unchanged_W=premise_embedding.unchanged_W,
+                                         unchanged_W_shape=unchanged_W_shape,
+                                         oov_in_train_W=premise_embedding.oov_in_train_W,
+                                         oov_in_train_W_shape=oov_in_train_W_shape,
+                                         p=self.p,
+                                         dropout_mask=premise_embedding.dropout_mask)
+        hypo_embedding = FakeFeatureDot2Layer(hypo_embedding)
+        mlstm = MatchLSTM(hypo_embedding, self.k, peepholes=False, mask_input=l_hypo_mask,
+                          encoder_input=premise_embedding, encoder_mask_input=l_premise_mask,
+                          )
+        self.p = 0.
+        if self.p > 0.:
+            print('apply dropout rate {} to decoder'.format(p))
+            mlstm = lasagne.layers.DropoutLayer(mlstm, p)
+        l_softmax = lasagne.layers.DenseLayer(
+                mlstm, num_units=3,
+                nonlinearity=lasagne.nonlinearities.softmax)
+        print('loading pre-trained model ...')
+        # And load them again later on like this:
+        with np.load(self.model_file) as f:
+            param_values = [f['arr_%d' % i] for i in range(len(f.files))]
+        lasagne.layers.set_all_param_values(l_softmax, param_values)
+
+        target_var = T.ivector('target_var')
+
+        test_prediction = lasagne.layers.get_output(l_softmax, deterministic=True)
+        test_loss = lasagne.objectives.categorical_crossentropy(test_prediction,
+                                                                target_var)
+        test_loss = test_loss.mean()
+        # lasagne.objectives.categorical_accuracy()
+        # As a bonus, also create an expression for the classification accuracy:
+        test_predict_cls = T.argmax(test_prediction, axis=1)
+        test_acc = T.mean(T.eq(test_predict_cls, target_var),
+                          dtype=theano.config.floatX)
+
+        # Theano functions for training and computing cost
+        print("Compiling functions ...")
+        self.predict_fn = theano.function([premise_var, premise_mask, hypo_var, hypo_mask],
+                                     test_predict_cls)
+
+    def train(self):
+        pass
+
+    def predict(self, samples):
+        """
+        prediction function
+        :param samples: list of tuple(premise, hypothesis)
+        :return:
+        """
+        (premise, premise_masks, hypothesis, hypothesis_masks) = self.prepare(samples)
+        predicted_class = self.predict_fn(premise, premise_masks, hypothesis, hypothesis_masks)
+        for name in map(lambda l: self.LABEL2NAME[l], predicted_class):
+            print(name)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('usage: {} sample_file'.format(sys.argv[0]))
+        sys.exit(0)
+    sample_file = sys.argv[1]
+    samples = []
+    with open(sample_file, 'r') as f:
+        lines = f.readlines()
+        for i in range(0, len(lines), 2):
+            p = lines[i]
+            h = lines[i+1]
+            print((p, h))
+            samples.append((p, h))
+
+    model = MatchLSTMModel()
+    model.predict(samples)
--- test_snli_match_lstm_current_acc.py
+++ test_snli_match_lstm_current_acc.py
@@ -71,18 +71,7 @@ def prepare(df):
 # In[3]:
 
 print('Loading data ...')
-train_df, dev_df, test_df = (None, None, None)
-with open('./snli/converted_train.pkl', 'rb') as f:
-    print('Loading train ...')
-    train_df = pickle.load(f)
-    print(len(train_df))
-    filtered_s2 = train_df.sentence2.apply(lambda s2: len(s2) != 0)
-    train_df = train_df[filtered_s2]
-    print(len(train_df))
-    train_df = train_df[train_df.gold_label != '-']
-    train_df = train_df.reset_index()
-    print(len(train_df))
-    # prepare(train_df)
+dev_df, test_df = (None, None)
 with open('./snli/converted_dev.pkl', 'rb') as f:
     print('Loading dev ...')
     dev_df = pickle.load(f)
@@ -111,11 +100,10 @@ hypothesis_max = 62 + 1
 
 # In[8]:
 
-def main(num_epochs=10, k=300, batch_size=30,
+def main(k=300, batch_size=30,
          display_freq=100,
          save_freq=1000,
          load_previous=False):
-    print('num_epochs: {}'.format(num_epochs))
     print('k: {}'.format(k))
     print('batch_size: {}'.format(batch_size))
     print('display_frequency: {}'.format(display_freq))
@@ -165,7 +153,7 @@ def main(num_epochs=10, k=300, batch_size=30,
     p = 0.
     if p > 0.:
         print('apply dropout rate {} to decoder'.format(p))
-        decoder = lasagne.layers.DropoutLayer(mlstm, p)
+        mlstm = lasagne.layers.DropoutLayer(mlstm, p)
     l_softmax = lasagne.layers.DenseLayer(
             mlstm, num_units=3,
             nonlinearity=lasagne.nonlinearities.softmax)
@@ -178,26 +166,7 @@ def main(num_epochs=10, k=300, batch_size=30,
 
     target_var = T.ivector('target_var')
 
-    # lasagne.layers.get_output produces a variable for the output of the net
-    prediction = lasagne.layers.get_output(l_softmax, deterministic=False)
-    # The network output will have shape (n_batch, 3);
-    loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
-    cost = loss.mean()
-    if l2_weight > 0.:
-        # apply l2 regularization
-        print('apply l2 penalty to encoder and decoder, weight: {}'.format(l2_weight))
-        regularized_layers = {mlstm: l2_weight}
-        l2_penalty = lasagne.regularization.regularize_layer_params_weighted(
-                regularized_layers,
-                lasagne.regularization.l2)
-        cost += l2_penalty
-    # Retrieve all parameters from the network
-    all_params = lasagne.layers.get_all_params(l_softmax, trainable=True)
-    # Compute adam updates for training
-    print("Computing updates ...")
-    updates = lasagne.updates.adam(cost, all_params, learning_rate=learning_rate)
-
-    test_prediction, test_mlstm_out = lasagne.layers.get_output([l_softmax, mlstm], deterministic=True)
+    test_prediction = lasagne.layers.get_output(l_softmax, deterministic=True)
     test_loss = lasagne.objectives.categorical_crossentropy(test_prediction,
                                                             target_var)
     test_loss = test_loss.mean()
@@ -209,114 +178,45 @@ def main(num_epochs=10, k=300, batch_size=30,
 
     # Theano functions for training and computing cost
     print("Compiling functions ...")
-    train_fn = theano.function([premise_var, premise_mask, hypo_var, hypo_mask, target_var],
-                               cost, updates=updates)
     val_fn = theano.function([premise_var, premise_mask, hypo_var, hypo_mask, target_var],
                              [test_loss, test_acc])
     predict_fn = theano.function([premise_var, premise_mask, hypo_var, hypo_mask],
-                                 [test_predict_cls, test_mlstm_out])
+                                 test_predict_cls)
     print("Testing ...")
-    print('train_df.shape: {0}'.format(train_df.shape))
     print('dev_df.shape: {0}'.format(dev_df.shape))
     print('test_df.shape: {0}'.format(test_df.shape))
-    base_filename = './ensemble_out/mlstm_'
     try:
         print("Starting evaluating...")
         # And a full pass over the validation data:
         val_err = 0
         val_acc = 0
         val_batches = 0
-        val_predicts = []
-        last_hidden_out = []
-        all_labels = []
         for start_i in range(0, len(dev_df), batch_size):
             batched_df = dev_df[start_i:start_i + batch_size]
             ps, p_masks, hs, h_masks, labels = prepare(batched_df)
             err, acc = val_fn(ps, p_masks, hs, h_masks, labels)
-            predicted_cls, mlstm_out = predict_fn(ps, p_masks, hs, h_masks)
-            val_predicts.append(predicted_cls)
-            last_hidden_out.append(mlstm_out)
-            all_labels.append(labels)
             val_err += err
             val_acc += acc
             val_batches += 1
         print("  validation loss:\t\t{:.6f}".format(val_err / val_batches))
         print("  validation accuracy:\t\t{:.2f} %".format(
                 val_acc / val_batches * 100))
-        val_predicts = np.concatenate(val_predicts)
-        last_hidden_out = np.concatenate(last_hidden_out)
-        print('last_hidden_out.shape: {}'.format(last_hidden_out.shape))
-        all_labels = np.concatenate(all_labels)
-        print('all_labels.shape: {}'.format(all_labels.shape))
-        np.savez(base_filename + 'dev.npz', last_hidden_out=last_hidden_out, all_labels=all_labels)
 
         # After training, we compute and print the test error:
         test_err = 0
         test_acc = 0
         test_batches = 0
-        test_predicts = []
-        last_hidden_out = []
-        all_labels = []
+
         for start_i in range(0, len(test_df), batch_size):
             batched_df = test_df[start_i:start_i + batch_size]
             ps, p_masks, hs, h_masks, labels = prepare(batched_df)
             err, acc = val_fn(ps, p_masks, hs, h_masks, labels)
-            predicted_cls, mlstm_out = predict_fn(ps, p_masks, hs, h_masks)
-            test_predicts.append(predicted_cls)
-            last_hidden_out.append(mlstm_out)
-            all_labels.append(labels)
             test_err += err
             test_acc += acc
             test_batches += 1
         print("  test loss:\t\t\t{:.6f}".format(test_err / test_batches))
         print("  test accuracy:\t\t{:.2f} %".format(
                 test_acc / test_batches * 100))
-        test_predicts = np.concatenate(test_predicts)
-        last_hidden_out = np.concatenate(last_hidden_out)
-        print('last_hidden_out.shape: {}'.format(last_hidden_out.shape))
-        all_labels = np.concatenate(all_labels)
-        print('all_labels.shape: {}'.format(all_labels.shape))
-        np.savez(base_filename + 'test.npz', last_hidden_out=last_hidden_out, all_labels=all_labels)
-
-        # save
-        # print('saving predicts results to file ...')
-        # np.savez('./snli/mlstm_predict.npz', val_predicts=val_predicts,
-        #          test_predicts=test_predicts)
-
-        # Finally, launch the training loop.
-        train_err = 0
-        train_acc = 0
-        train_batches = 0
-        display_at = time.time()
-        train_predicts = []
-        last_hidden_out = []
-        all_labels = []
-        for start_i in range(0, len(train_df), batch_size):
-            batched_df = train_df[start_i:start_i + batch_size]
-            ps, p_masks, hs, h_masks, labels = prepare(batched_df)
-            err, acc = val_fn(ps, p_masks, hs, h_masks, labels)
-            predicted_cls, mlstm_out = predict_fn(ps, p_masks, hs, h_masks)
-            train_predicts.append(predicted_cls)
-            last_hidden_out.append(mlstm_out)
-            all_labels.append(labels)
-            train_acc += acc
-            train_err += err
-            train_batches += 1
-            # display
-            if train_batches % display_freq == 0:
-                print("Seen {:d} samples, time used: {:.3f}s".format(
-                        train_batches * batch_size, time.time() - display_at))
-                print("  current training loss:\t\t{:.6f}".format(train_err / train_batches))
-                print("  current training accuracy:\t\t{:.6f}".format(train_acc / train_batches))
-                display_at = time.time()
-        print("  training loss:\t\t{:.6f}".format(train_err / train_batches))
-        print("  training accuracy:\t\t{:.2f} %".format(
-                train_acc / train_batches * 100))
-        last_hidden_out = np.concatenate(last_hidden_out)
-        print('last_hidden_out.shape: {}'.format(last_hidden_out.shape))
-        all_labels = np.concatenate(all_labels)
-        print('all_labels.shape: {}'.format(all_labels.shape))
-        np.savez(base_filename + 'train.npz', last_hidden_out=last_hidden_out, all_labels=all_labels)
 
         # Optionally, you could now dump the network weights to a file like this:
         # np.savez('model.npz', *lasagne.layers.get_all_param_values(network))
@@ -333,5 +233,5 @@ def main(num_epochs=10, k=300, batch_size=30,
 
 if __name__ == '__main__':
 
-    main(num_epochs=20, batch_size=30, load_previous=True)
+    main(batch_size=30, load_previous=True)
 
